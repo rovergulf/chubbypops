358a378e063d3a702a4a18ddfb86bb01e4bc4cd8
Dmitrii Limonov <disordervlz@gmail.com>
2021-11-06 03:03:37 +0300

    add favicons style fixes update assets add robots.txt add sitemap.xml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    contract: '0xb839818b11315fd10f181b43b5ccb935a03dae1d',
+    contract: '???',
     templateId: '5ffc02e5-656c-46b3-b5ff-fc2254576d3e',
-    apiUrl: `https://api.rovergulf.net/nft/templates/5ffc02e5-656c-46b3-b5ff-fc2254576d3e/factory`,
+    apiUrl: `https://api.rovergulf.net/nft/templates/???/factory`,
     cdn: 'https://storage.googleapis.com/rovergulf/nft-gen/main',
 };
