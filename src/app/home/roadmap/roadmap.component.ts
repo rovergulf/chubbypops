fecfb9cff0040373ee9c82049d3f8f0ab9653f64
Dmitrii Limonov <disordervlz@gmail.com>
2022-01-25 18:47:43 +0300

    Update roadmap

--- src/app/home/roadmap/roadmap.component.ts
+++ src/app/home/roadmap/roadmap.component.ts
@@ -29,23 +29,23 @@ export class RoadmapComponent implements OnInit {
         point: '25%',
         image: '/assets/images/25.svg',
         content: [
-            'Good deeds bank fills with 5 ETH',
+            'Good deeds bank fills with 60% profits',
             'Choosing a fund for donation by voting in discord and transfer of funds'
         ]
     }, {
         point: '50%',
         image: '/assets/images/50.svg',
         content: [
-            'Good deeds bank fills with 5 ETH',
+            'Good deeds bank fills with 60% profits',
             'Choosing a fund for donation by voting in discord and transfer of funds',
-            'Drawing of 1 ETH among holders',
+            'Drawing of 3 ETH among holders',
             'List collection on rarity.tools',
         ]
     }, {
         point: '75%',
         image: '/assets/images/75.svg',
         content: [
-            'Good deeds bank fills with 8 ETH',
+            'Good deeds bank fills with 60% profits',
             'Choosing a fund for donation by voting in discord and transfer of funds',
             'Exclusive NFTs will be raffled among owners',
         ]
@@ -53,7 +53,7 @@ export class RoadmapComponent implements OnInit {
         point: '100%',
         image: '/assets/images/100.svg',
         content: [
-            'Good deeds bank fills with 10 ETH',
+            'Good deeds bank fills with 60% profits',
             'Choosing a fund for donation by voting in discord and transfer of funds',
             'Creators make themselves bird tattoos',
         ]
